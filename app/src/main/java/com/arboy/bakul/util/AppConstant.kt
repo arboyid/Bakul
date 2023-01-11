037eabb771e65990617740f6e473e8bc35493065
Mardiansyah I. E Gunting <arboy@arboy.id>
2023-01-11 16:21:55 +0700

    initial init

--- app/src/main/java/com/arboy/bakul/util/AppConstant.kt
+++ app/src/main/java/com/arboy/bakul/util/AppConstant.kt
@@ -3,7 +3,7 @@ package com.arboy.bakul.util
 object AppConstant {
 
     const val LENGTH_SPLASH = 3000L;
-    const val SALDO_MASUK = 100000;
+    const val SALDO_MASUK = 0;
     const val KEY_INTENT = "transaction";
 
 }
